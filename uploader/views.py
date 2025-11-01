d18b37ca280805aa0da37fa412fc4f38456ad8db
Jai <jaikamath169@gmail.com>
2025-11-01 20:26:32 +0530

    fixing things

--- uploader/views.py
+++ uploader/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import UploadedFile
+from .models import UploadedFile
+from .forms import UploadFileForm
 
 def home(request):
     files = UploadedFile.objects.all().order_by('-uploaded_at')
@@ -11,16 +13,18 @@ def home(request):
 def is_uploader(user):
     return user.is_authenticated and user.groups.filter(name='uploader').exists()
 
-@user_passes_test(is_uploader, login_url='permission_denied')
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Uploader').exists(), login_url='/permission-denied/')
 def upload_file(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        file = request.FILES['file']
-        UploadedFile.objects.create(title=title, description=description, file=file)
-        messages.success(request, '✅ File uploaded successfully!')
-        return redirect('home')
-    return render(request, 'uploader/upload.html')
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UploadFileForm()
+    return render(request, 'uploader/upload.html', {'form': form})
 
 def permission_denied(request):
     return render(request, 'uploader/permission_denied.html')
